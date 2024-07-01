765621c703cfa5d11f4968f188b007f44cd5877b
Marlenezhang <89639372+Marlenezhang@users.noreply.github.com>
2024-07-01 20:34:39 +0800

    Update distance_multi_pop.sh
    Notes translated into English

--- shell/distance_multi_pop.sh
+++ shell/distance_multi_pop.sh
@@ -2,15 +2,15 @@
 
 
 ########################################################################################################################
-## 版本: 1.1.0
-## 作者: 李伟宁 liwn@cau.edu.cn
-## 日期: 2023-07-05
+## Version: 1.1.1
+## Author: Weining Li liwn@cau.edu.cn
+## Date: 2023-07-05
+##
+## Calculate the average genetic distance for the provided populations
 ## 
-## 对提供的任意多个群体进行平均遗传距离计算
+## Usage: distance_multi_pop.sh --help
 ## 
-## 使用: distance_multi_pop.sh --help
-## 
-## 依赖软件/环境:
+## Required software/environment:
 ## R
 ## plink (1.90)
 ## 
@@ -20,31 +20,31 @@
 ########################################################################################################################
 
 
-###################  参数处理  #####################
+###################  Parameter processing  #####################
 ####################################################
 ## NOTE: This requires GNU getopt.  On Mac OS X and FreeBSD, you have to install this
-## 参数名
+## Parameters
 TEMP=$(getopt -o h --long bfile:,out:,help \
               -n 'javawrap' -- "$@")
 if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
 eval set -- "$TEMP"
-## 解析参数
+## Parse parameters
 while true; do
   case "$1" in
-    --bfile )   bfile="$2";  shift 2 ;;  ## plink文件前缀，如"/public/home/popA /public/home/popB"
-    --nchr )    nchr="$2" ;  shift 2 ;;  ## 染色体数目 [30]
-    --out )     out="$2" ;   shift 2 ;;  ## 输出文件名
+    --bfile )   bfile="$2";  shift 2 ;;  ## PLINK file prefixes, e.g.,"/public/home/popA /public/home/popB"
+    --nchr )    nchr="$2" ;  shift 2 ;;  ## Number of chromosomes [30]
+    --out )     out="$2" ;   shift 2 ;;  ## Output filename
     -h | --help )     grep " shift " $0 && exit 1 ;;
     -- ) shift; break ;;
     * ) shift; break ;;
   esac
 done
 
-## 默认参数
+## Default parameters
 out=${out:="distance"}
 nchr=${nchr:="30"}
 
-## 脚本所在文件夹
+## Check the scripts directory
 if [[ ${code} ]]; then
   [[ ! -d ${code} ]] && echo "${code} not exists! " && exit 5
 else
@@ -52,29 +52,29 @@ else
   code=$(dirname "$script_path")
 fi
 
-## 脚本
+## Scripts
 dist=${code}/R/mean_distance.R
 func=${code}/shell/function.sh
 
-## 载入自定义函数
+## Load custom functions
 [[ ! -s ${func} ]] && echo "Error: ${func} not found! " && exit 5
 source ${func}
 
-## 检查plink二进制文件是否存在
+## Check if PLINK files exist
 check_plink "${bfile}" ${nchr}
 
-## 工作文件夹
+## Working directory
 dir=$(dirname ${bfile})
 cd ${dir} || exit
 
-## 个体按照FID排序
+## Sort individuals by FID
 plink --bfile ${bfile} --chr-set ${nchr} --indiv-sort natural --make-bed --out distance_tmp
 
-## 计算距离矩阵
+## Sort individuals by FID
 plink --bfile distance_tmp --chr-set ${nchr} --distance square0 1-ibs --out distance_tmp
 
-## 计算矩阵中品种配对对应的块的均值
+## Calculate the mean of blocks corresponding to breed pairs in the matrix
 $dist --prefix distance_tmp --out ${out}
 
-## 删除中间文件
+## Remove intermediate files
 rm distance_tmp.*
